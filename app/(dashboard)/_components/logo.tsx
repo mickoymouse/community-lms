7b26b83fe2b45c3ed1e4bae06e2511ea31578e10
mickoymouse <mckslvstr@gmail.com>
2023-09-21 15:28:02 +0800

    Added initial navbar and sidebar.

--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -3,8 +3,8 @@ import Image from 'next/image';
 const Logo = () => {
     return ( 
         <Image 
-            height={130}
-            width={130}
+            height={100}
+            width={100}
             alt='logo'
             src="/logo.svg"
         />
