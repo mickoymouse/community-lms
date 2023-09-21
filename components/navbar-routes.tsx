90ffcc3fa40e5fab5230035cb8b180689c6a01dc
mickoymouse <mckslvstr@gmail.com>
2023-09-21 15:43:25 +0800

    Added initial switching and landing page for teacher mode.

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,11 +1,35 @@
 "use client"
 
 import { UserButton } from "@clerk/nextjs";
+import { usePathname } from "next/navigation";
+import { Button } from "./ui/button";
+import { LogOut } from "lucide-react";
+import Link from "next/link";
 
 const NavbarRoutes = () => {
+    const pathName = usePathname()
+
+    const isTeacherPage = pathName.startsWith("/teacher")
+    const isChapterPage = pathName.startsWith("/chapter")
+
     return ( 
         <div className="flex gap-x-2 ml-auto">
-            <UserButton />
+            {isTeacherPage || isChapterPage ? (
+                <Link href="/">
+                    <Button size="sm" variant="ghost">
+                        <LogOut className="h-4 w-4 mr-2" />
+                        Exit    
+                    </Button>
+                </Link>
+            ) :
+            (
+                <Link href="/teacher/courses">
+                    <Button size="sm" variant="ghost">
+                        Teacher Mode
+                    </Button>
+                </Link>
+            )}
+            <UserButton afterSignOutUrl="/"/>
         </div>
      );
 }
