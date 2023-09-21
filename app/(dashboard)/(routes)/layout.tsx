6622958789f7110ec0fddd2b837bd6fbcfaa1def
mickoymouse <mckslvstr@gmail.com>
2023-09-21 12:27:43 +0800

    Added sidebar implementation.

--- app/(dashboard)/(routes)/layout.tsx
+++ app/(dashboard)/(routes)/layout.tsx
@@ -10,7 +10,9 @@ const DashboardLayout = ({
             <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
                 <Sidebar />
             </div>
-            {children}
+            <main className="md:pl-56 h-full">
+                {children}
+            </main>
         </div>
      );
 }
