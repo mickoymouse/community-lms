e622566425866a818059a1dd1810c3de51e3ad86
mickoymouse <mckslvstr@gmail.com>
2023-09-21 10:54:13 +0800

    Initial commit.

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>
+      <p className='text-3xl font-medium text-sky-700'>Hello World!</p>
+      <Button>Click me</Button>
+    </div>
+  )
+}
