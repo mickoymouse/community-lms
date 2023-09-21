7ff58b446fe01945d6cd32ec19b0a1378136776f
mickoymouse <mckslvstr@gmail.com>
2023-09-21 12:10:49 +0800

    Initialize clerk for user management.

---
+++ app/(dashboard)/_components/sidebar-item.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import { LucideIcon } from "lucide-react";
+
+interface SidebarItemProps {
+    icon: LucideIcon,
+    label: string,
+    href: string,
+}
+
+const SidebarItem = ({
+    icon,
+    label,
+    href,
+}: SidebarItemProps) => {
+    return ( 
+        <div>o</div>
+     );
+}
+ 
+export default SidebarItem;
