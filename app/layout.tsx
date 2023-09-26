e6d533095a8c47400e83f7a9588930098d99f2a1
mickoymouse <mckslvstr@gmail.com>
2023-09-26 16:22:07 +0800

    Finished up course creation.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,31 @@
-import { ClerkProvider } from '@clerk/nextjs'
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import ToastProvider from '@/components/providers/toast-provider'
+import { ClerkProvider } from "@clerk/nextjs";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import ToastProvider from "@/components/providers/toast-provider";
+import { ConfettiProvider } from "@/components/providers/confetti-provider";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Community LMS',
-  description: 'Find free and open source coding materials for your community.',
-}
+	title: "Community LMS",
+	description: "Find free and open source coding materials for your community.",
+};
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode;
 }) {
-  return (
-      <ClerkProvider>
-        <html lang="en">
-          <body className={inter.className}>
-            <ToastProvider />
-            {children}
-          </body>
-        </html>
-      </ClerkProvider>
-  )
+	return (
+		<ClerkProvider>
+			<html lang="en">
+				<body className={inter.className}>
+					<ConfettiProvider />
+					<ToastProvider />
+					{children}
+				</body>
+			</html>
+		</ClerkProvider>
+	);
 }
